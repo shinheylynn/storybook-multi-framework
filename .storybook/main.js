16e22435d157cff62825f5c0e0e251cd1e794032
shinheylynn <heylub@gmail.com>
2023-08-16 17:26:41 +0900

    Modify: delete docs file in vue-storybook stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,27 +1,32 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
-  refs: {
-    react: {
-      title: "React",
-      url: "https://64dc5abb16a2891a6721876e-eizcjvniri.chromatic.com",
-    },
-  },
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+	stories: [
+		"../stories/**/*.mdx",
+		"../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+	],
+	refs: {
+		react: {
+			title: "React",
+			url: "https://64dc8351623e3a642cdd9b84-qfrpzeixoa.chromatic.com",
+		},
+	},
+	refs: {
+		vue: {
+			title: "Vue",
+			url: "https://64dc812a8d1c331facc2ed4f-qzekdnijrv.chromatic.com",
+		},
+	},
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions",
+	],
+	framework: {
+		name: "@storybook/react-vite",
+		options: {},
+	},
+	docs: {
+		autodocs: "tag",
+	},
 };
 export default config;
