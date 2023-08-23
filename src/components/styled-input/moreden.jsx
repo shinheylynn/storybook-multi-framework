bdcb142125d8450af3386f87ebf6edab8b8c2679
shinheylynn <heylub@gmail.com>
2023-08-23 15:21:36 +0900

    Add: StyledInput react 기본 토대 추가

--- src/components/styled-input/moreden.jsx
+++ src/components/styled-input/moreden.jsx
@@ -3,14 +3,7 @@ import "./moreden.scss";
 import PropTypes from "prop-types";
 
 export const StyledInput = (props) => {
-	const {
-		Name,
-		Label,
-		Placeholder,
-		Comments,
-		Disabled,
-		TextAlign = "left",
-	} = props;
+	const { Name, Label, Placeholder, Comments, Disabled } = props;
 	const [isFocused, setIsFocused] = useState(false);
 
 	const inputFocused = () => {
@@ -19,7 +12,7 @@ export const StyledInput = (props) => {
 
 	return (
 		<div className="styled-input">
-			<div className={`"default-input" ${Disabled ? "disabled" : ""}`}>
+			<div className={`default-input ${Disabled ? "disabled" : ""}`}>
 				<div className="input-wrapper">
 					<label htmlFor={Name} className={Disabled ? "disabled" : ""}>
 						{Label}
@@ -28,45 +21,33 @@ export const StyledInput = (props) => {
 					<div className="input-container">
 						<input
 							autoComplete="off"
-							onKeyDown={(e) => {
-								props.onKeyDown(e);
-							}}
-							onKeyUp={(e) => {
-								props.onKeyUp(e);
-							}}
-							onFocus={(e) => {
-								props.onFocus(e);
-							}}
-							onBlur={(e) => {
-								props.onBlur(e);
-							}}
 							id={Name}
 							placeholder={Placeholder}
 							disabled={Disabled}
 						></input>
-						<span className="input-extention">
-							<span className="state-icon with-label"></span>
-							<span className="state-icon with-label"></span>
-							<span className="with-label">
-								<button
-									style={{
-										backgroundColor: "black",
-										color: "white",
-										width: 38 + "px",
-										height: 24 + "px",
-										border: "none",
-										fontSize: 12 + "px",
-										marginLeft: 5 + "px",
-										borderRadius: 3 + "px",
-									}}
-								>
-									버튼
-								</button>
-							</span>
-							{/* <div className="tooltip-wrapper"></div> */}
-						</span>
 					</div>
 				</div>
+				<span className="input-extention">
+					<span className="state-icon with-label"></span>
+					<span className="state-icon with-label"></span>
+					<span className="with-label">
+						<button
+							style={{
+								backgroundColor: "black",
+								color: "white",
+								width: 38 + "px",
+								height: 24 + "px",
+								border: "none",
+								fontSize: 12 + "px",
+								marginLeft: 5 + "px",
+								borderRadius: 3 + "px",
+							}}
+						>
+							버튼
+						</button>
+					</span>
+					<div className="tooltip-wrapper"></div>
+				</span>
 			</div>
 			{Comments && <small className="comments">{Comments}</small>}
 		</div>
@@ -79,5 +60,4 @@ StyledInput.propTypes = {
 	Placeholder: PropTypes.string,
 	Comments: PropTypes.string,
 	Disabled: PropTypes.bool,
-	TextAlgin: PropTypes.string,
 };
